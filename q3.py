ccfd37e0fb5c8ca9975fa52ae35b541c923b8236
Swaksy7781 <saurabhwakaseseb159@gmail.com>
2025-08-06 09:20:06 +0530

    added error and exception handling, output result.

--- q3.py
+++ q3.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 
-import os
+"""#Q3: Create a Directory (if not exists) Task: 
+Write a script that creates a new folder called test_folder in the current working directory if it doesn't already exist."""
 
-#Q3: Create a Directory (if not exists) Task: Write a script that creates a new folder called test_folder in the current working directory if it doesn't already exist.
+import os
 
 def new_directory():
     print(f"Current working directory is: {os.getcwd()}")
 
-    if (not os.path.exists("test_folder")):
-        os.mkdir("test_folder")
-        print(f"New directory named as 'test_folder' is created at {os.getcwd()}")
-    else:
-        print("directory already exists")
+    try:
+        if (not os.path.exists("test_folder")):
+            os.mkdir("test_folder")
+            print(f"New directory named as 'test_folder' is created at {os.getcwd()}")
+        else:
+            print("directory already exists")
+
+    except Exception as e:
+        # Handle any other unexpected errors
+        print(f"An unexpected error occurred: {e}")
 
 def main():
     new_directory()
 
 if __name__ == "__main__":
     main()
+
